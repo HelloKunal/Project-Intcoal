7338497d11a0256fb646e96a1f94f50c919316ee
Kunal <hellokunalpawar@gmail.com>
2021-06-01 09:44:53 +0530

    Started development. changed html to match goal

--- Icing/settings.py
+++ Icing/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'Icing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#if ON_HEROKU:
+#    DATABASE_URL = 'postgresql://<postgresql>'
+#else:
+#DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+#DATABASES = {
+#    'default': dj_database_url.config(default=DATABASE_URL)
+#}
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
